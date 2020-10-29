656d24fa9c192b56156807392c0a71f962bee731
lol <sp432735@gmail.com>
2020-10-28 20:21:27 -0400

    first commit

---
+++ fzgvd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fzgvd
+//
+//  Created by Alex Perz on 10/16/20.
+//  Copyright © 2020 Alex Perz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
